9352b7446d92f89229c41bb31747213ba043a590
Igor Borges <ibcastelheira1995@gmail.com>
2025-10-05 16:23:25 -0300

    updating postcss configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   css: ['@/assets/css/tailwind.css'],
   modules: ['@nuxtjs/tailwindcss'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {}
+    }
+  },
   app: {
     head: {
       title: 'Igor Borges | Portfolio',
